88121d8d795af3904ea339cdc5c3eaefad470157
Alexander Glück <github@alexanderglueck.at>
2022-03-26 14:04:58 +0100

    Fix wrong check duration

--- app/Console/Commands/CheckScrobbles.php
+++ app/Console/Commands/CheckScrobbles.php
@@ -64,7 +64,7 @@ class CheckScrobbles extends Command
             $lastPlayedTimestamp = $jsonResponse['recenttracks']['track'][0]['date']['uts'];
 
             $carbon = Carbon::createFromTimestampUTC($lastPlayedTimestamp);
-            if ($carbon->greaterThan(now()->subHours(3))) {
+            if ($carbon->greaterThan(now()->subHours(24))) {
                 // A song was played within the last 24 hours
                 continue;
             }
